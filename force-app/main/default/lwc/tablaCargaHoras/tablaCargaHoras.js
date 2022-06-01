c4223986c4d37d1b7847623ebf5709832a0df28d
Matiaslb420 <matiasluisb13@gmail.com>
2022-06-01 12:58:52 -0300

    realiza cambios en la db

--- force-app/main/default/lwc/tablaCargaHoras/tablaCargaHoras.js
+++ force-app/main/default/lwc/tablaCargaHoras/tablaCargaHoras.js
@@ -1,6 +1,7 @@
 import { api, LightningElement, track, wire } from 'lwc';
 import Id from '@salesforce/user/Id';
 import getResourceTasks from '@salesforce/apex/getTasksInProgress.getResourceTasks';
+import initializeTaskApex from '@salesforce/apex/taskTableButtons.initializeTask';
 
 export default class BasicDatatable extends LightningElement {
     @track tasksByProjectName = [];
@@ -17,4 +18,31 @@ export default class BasicDatatable extends LightningElement {
             console.log('error',error);
         }
     };
+
+    initializeTask(e){
+        initializeTaskApex({taskIdStr:e.target.dataset.id})
+        .then(task =>{
+            console.log('click');
+            console.log('task', task);
+            let index = 0;
+            let found = false;
+            while(index < this.tasksByProjectName.length && !found){
+                if(e.target.dataset.projectname === this.tasksByProjectName[index].key){
+                    found = true;
+                    let taskIndex = 0;
+                    for(let task in this.tasksByProjectName[index].value){
+                        if(e.target.dataset.id === task.Id){
+                            this.tasksByProjectName[index].value[taskIndex].State__c = 'In Progress';
+                            break;
+                            taskIndex++;
+                        }
+                    }
+                }
+                index++;
+            }
+            
+        })
+        .catch(console.error)
+        
+    }
 }
